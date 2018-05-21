5608696c4211f8c8aa0184a40497ff78df0d15bc
trashpony <rfhnjirf1>
2018-05-21 16:22:53 +0300

    log

--- src/Log/Log.go
+++ src/Log/Log.go
@@ -9,9 +9,9 @@ import (
 func Write(weightBox, widthBox, heightBox, lengthBox int)  {
 	currentTime := time.Now().Local()
 
-	f, err := os.OpenFile(currentTime.Format("2006-01-02"), os.O_APPEND|os.O_WRONLY, 0600)
+	f, err := os.OpenFile("./" + currentTime.Format("2006-01-02"), os.O_APPEND|os.O_WRONLY, 0600)
 	if err != nil {
-		f, _ = os.Create(currentTime.Format("2006-01-02"))
+		f, _ = os.Create("./" + currentTime.Format("2006-01-02"))
 	}
 
 	f.WriteString(currentTime.Format("15:04:05") + " - " + " weightBox: " + strconv.Itoa(weightBox) +
