16e9c50e98656f8abdba56ab4d6e00be1d896666
TrashPony <Rfhnjirf13>
2019-10-17 15:11:56 +0300

    ардуино код

--- arduino/laser.ino
+++ arduino/laser.ino
@@ -22,29 +22,17 @@ TroykaI2CHub splitter;
 #define RED_LED_PIN  9
 #define GREEN_LED_PIN  5
 
-#define TOP_MAX          77
-#define WIDTH_MAX        87
-#define LENGTH_MAX       46
+#define TOP_MAX          29
+#define WIDTH_MAX        20
+#define LENGTH_MAX       19
 
 boolean onlyWeight = false;
-
-boolean passWidthBox = false;
-boolean passHeightBox = false;
-boolean passLengthBox = false;
-
-int lastWidthBox;
-int lastHeightBox;
-int lastLengthBox;
 
 int widthBox;
 int heightBox;
 int lengthBox;
 
-int queueIndicationsWidth = 0;
-int queueIndicationsHeight = 0;
-int queueIndicationsLength = 0;
-
-boolean debug = true;
+boolean debug = false;
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
@@ -57,7 +45,7 @@ void setup()
   pinMode(GREEN_LED_PIN, OUTPUT);
   pinMode(BUTTON, INPUT);
 
-  Serial.begin(115200);
+  Serial.begin(19200);
   // ждем пока откроется сериал порт
   while(!Serial) {}
 
@@ -80,7 +68,7 @@ void loop()
       digitalWrite(RED_LED_PIN, LOW);
       digitalWrite(GREEN_LED_PIN, HIGH);
     }
-    delay(500);
+    //delay(500);
   }
 
   Indication();
@@ -90,83 +78,26 @@ void loop()
     byte incomingByte = Serial.read();
 
     if (incomingByte == 0x95) {
-      Serial.write(0x7F);
-      Serial.write(0x7F);
-      Serial.write(0x7F);
-      Serial.write(0x7F);
-      Serial.write(0x7F);
+      byte buf[4] = {0x7F, 0x7F, 0x7F, 0x7F};
+      Serial.write(buf, 4);
       Serial.flush();
     }
 
-    // запрос ширины
+    // запрос габаритов
     if (incomingByte == 0x88) {
-      Serial.write(0x2D);
-
-      if (onlyWeight) {
-        Serial.write(0x7A);
-      }
-      else {
-        if (passWidthBox) {
-          Serial.write(0x7F);
-        }
-        else {
-          Serial.write(0x7E);
-        }
-      }
-
-      Serial.write(0x0B);
-      Serial.write(widthBox);
-      Serial.write(0x7B);
-    }
-
-    // запрос высоты
-    if (incomingByte == 0x99) {
-      Serial.write(0x2D);
-
-      if (onlyWeight) {
-        Serial.write(0x7A);
-      }
-      else {
-        if (passHeightBox) {
-          Serial.write(0x7F);
-        }
-        else {
-          Serial.write(0x7E);
-        }
-      }
-
-      Serial.write(0x16);
-      Serial.write(heightBox);
-      Serial.write(0x7B);
-    }
-
-    // запрос длины
-    if (incomingByte == 0x77) {
-      Serial.write(0x2D);
-
-      if (onlyWeight) {
-        Serial.write(0x7A);
-      }
-      else {
-        if (passLengthBox) {
-          Serial.write(0x7F);
-        }
-        else {
-          Serial.write(0x7E);
-        }
-      }
-
-      Serial.write(0x21);
-      Serial.write(lengthBox);
-      Serial.write(0x7B);
+      byte buf[12] = {
+        0x2D, 0x0B, widthBox, 0x7B,
+        0x2D, 0x16, heightBox, 0x7B,
+        0x2D, 0x21, lengthBox, 0x7B,
+        };
+      Serial.write(buf, 12);
     }
 
     if (incomingByte == 0x66) {
       if (onlyWeight) {
         digitalWrite(GREEN_LED_PIN, HIGH);
         digitalWrite(RED_LED_PIN, LOW);
-      }
-      else {
+      } else {
         digitalWrite(RED_LED_PIN, HIGH);
         digitalWrite(GREEN_LED_PIN, LOW);
       }
@@ -183,27 +114,27 @@ int getDistance(int pin) {
 
   // pin - указываем номер сети для лазера откуда брать данные
   splitter.setBusChannel(pin);
-  
+
   return getIndication();
 }
 
 int getIndication() {
-  
+
   VL53L0X_RangingMeasurementData_t measure;
   lox.rangingTest(&measure, false);
 
-  // проверка на доступность устройства 
+  // проверка на доступность устройства
   Wire.beginTransmission(LOX2_ADDRESS);
   byte state = Wire.endTransmission();
 
   float dist = round((measure.RangeMilliMeter)/10);
   int distInt = int(dist);
-  
+
   if (measure.RangeStatus != 4 && state == 0) {
     return distInt;
   } else {
     return -1;
-  }   
+  }
 }
 
 void Indication() {
@@ -213,29 +144,9 @@ void Indication() {
   int top =  getDistance(TOP_PING_LAN);
   int back = getDistance(BACK_PING_LAN);
 
-  if (right > 0 && left > 0 && top > 0 && back > 0) {
-
-    widthBox = WIDTH_MAX - (right + left);
-    heightBox = TOP_MAX - top;
-    lengthBox = LENGTH_MAX - back;
-
-    passWidthBox = PassedIndication(lastWidthBox, widthBox, queueIndicationsWidth);
-    passHeightBox = PassedIndication(lastHeightBox, heightBox, queueIndicationsHeight);
-    passLengthBox = PassedIndication(lastLengthBox, lengthBox, queueIndicationsLength);
-
-  } else {
-    passWidthBox = false;
-    passHeightBox = false;
-    passLengthBox = false;
-
-    lastWidthBox = 0;
-    lastHeightBox = 0;
-    lastLengthBox = 0;
-
-    widthBox = 0;
-    heightBox = 0;
-    lengthBox = 0;
-  }
+  widthBox = WIDTH_MAX - (right + left);
+  heightBox = TOP_MAX - top;
+  lengthBox = LENGTH_MAX - back;
 
   if (debug) {
     Serial.print("Right_ping: ");
@@ -257,56 +168,18 @@ void Indication() {
     Serial.println("///////////////////////////////////////");
 
     Serial.print("widthBox ");
-    Serial.print(passWidthBox);
     Serial.print(" ");
     Serial.println(widthBox);
 
     Serial.print("heightBox ");
-    Serial.print(passHeightBox);
     Serial.print(" ");
     Serial.println(heightBox);
 
     Serial.print("lengthBox ");
-    Serial.print(passLengthBox);
     Serial.print(" ");
     Serial.println(lengthBox);
 
     Serial.println("///////////////////////////////////////");
     delay(2500);
-  }
-}
-
-boolean PassedIndication (int &lastIndication, int &indication, int &queueIndications) {
-
-  if (debug) {
-    Serial.print("queueIndications ");
-    Serial.println(queueIndications);
-
-    Serial.print("lastIndication ");
-    Serial.println(lastIndication);
-
-    Serial.print("indication ");
-    Serial.println(indication);
-  }
-
-  if (indication > 0) {
-    if ((lastIndication - 1) <= indication && indication <= (lastIndication + 1)) {
-      if (queueIndications >= 5) {
-        return true;
-      }
-      else {
-        queueIndications++;
-        return false;
-      }
-    }
-    else {
-      queueIndications = 0;
-      lastIndication = indication;
-      return false;
-    }
-  }
-  else {
-    queueIndications = 0;
-    return false;
   }
 }
